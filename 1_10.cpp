58d05d218daf4162931b85936a99917a65e6f57e
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-08-26 15:42:53 -0700

    Update 1_10.cpp

--- 1_10.cpp
+++ 1_10.cpp
@@ -1,1 +1,43 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            August 26, 2018
+** Description:     10. Using constructor with default arguments
+**
+**
+*********************************************************************/
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
+class Airplane{
+    private:
+        string type;
+        int speed;
+
+    public:
+        // default constructor with default values
+        Airplane(string model = "none", int velocity = 0)
+            : type{model}, speed{velocity} {
+        }
+
+        string getType() {
+            return type;
+        }
+        int getSpeed() {
+            return speed;
+        }
+};
+
+int main() {
+    Airplane plane {"SR-71", 1100};
+    cout << "Model: " << plane.getType() << " Speed: " << plane.getSpeed() <<  "MPH" << endl;
+
+    Airplane wrightPlane;
+    cout << "Model: " << wrightPlane.getType() << " Speed: "
+         << wrightPlane.getSpeed() <<  "MPH" << endl;
+
+    return 0;
+}
