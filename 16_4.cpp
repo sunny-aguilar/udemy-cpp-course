553b3d0083430e8cd69b78d5652fd765fbb005e0
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-13 15:59:02 -0700

    add main function

--- 16_4.cpp
+++ 16_4.cpp
@@ -3,4 +3,15 @@
 ** Date:            September 13, 2018
 ** Description:     Exceptions, Templates, and the STL
 *********************************************************************/
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
+int main() {
+
+
+    return 0;
+}
