811c8b38c54702d8e63749e0eadd34ebd9fb17e6
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-14 15:58:57 -0700

    added template function

--- 16_5.cpp
+++ 16_5.cpp
@@ -11,8 +11,14 @@ using std::endl;
 using std::string;
 using std::cerr;
 
+template <typename T>
+T square(T num1, T num2) {
+    return num1 * num2;
+}
+
 int main() {
 
+    cout << "Square is: " << square(2,2);
 
     return 0;
 }
