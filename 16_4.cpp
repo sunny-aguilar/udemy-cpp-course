44bcefd300c9dab74e3fe87689e65b181efa3df9
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-13 20:08:06 -0700

    added proto

--- 16_4.cpp
+++ 16_4.cpp
@@ -10,12 +10,18 @@ using std::cout;
 using std::endl;
 using std::string;
 
-
+int divide(int num, int den);
 
 int main() {
     int x = 1;
     int y = 0;
 
+    divide(3,4);
+
 
     return 0;
+}
+
+int divide(int num, int den) {
+
 }
