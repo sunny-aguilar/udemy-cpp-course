97ddd959edc26a09bfb04d329d7037763a7215f0
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-14 16:17:43 -0700

    fixed bug

--- 16_4.cpp
+++ 16_4.cpp
@@ -1,45 +1,45 @@
-/*********************************************************************
-** Author:          Sandro Aguilar
-** Date:            September 13, 2018
-** Description:     Exceptions, Templates, and the STL
-*********************************************************************/
-#include <iostream>
-#include <string>
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::cerr;
-
-int divide(int num, int den);
-
-int main() {
-    double quotient{};
-
-    try {
-        quotient = divide(10,1);
-        cout << "The answer to the division is: " << quotient<< endl;
-    }
-    catch (string &ex) {
-        std::cerr << ex;
-    }
-    cout << "Program has completed normal ops" << endl;
-
-    return 0;
-}
-
-int divide(int num, int den) {
-    // add additional exception here
-    try {
-        if (num == 10)
-            throw 10;
-    }
-    catch (int ex) {
-        cerr << "Integer error caught!" << endl;
-    }
-    
-    if (den == 0)
-        throw string("Cannot divide by zero!\n");
-    else
-        return num / den;
-}
+///*********************************************************************
+//** Author:          Sandro Aguilar
+//** Date:            September 13, 2018
+//** Description:     Exceptions, Templates, and the STL
+//*********************************************************************/
+//#include <iostream>
+//#include <string>
+//using std::cin;
+//using std::cout;
+//using std::endl;
+//using std::string;
+//using std::cerr;
+//
+//int divide(int num, int den);
+//
+//int main() {
+//    double quotient{};
+//
+//    try {
+//        quotient = divide(10,1);
+//        cout << "The answer to the division is: " << quotient<< endl;
+//    }
+//    catch (string &ex) {
+//        std::cerr << ex;
+//    }
+//    cout << "Program has completed normal ops" << endl;
+//
+//    return 0;
+//}
+//
+//int divide(int num, int den) {
+//    // add additional exception here
+//    try {
+//        if (num == 10)
+//            throw 10;
+//    }
+//    catch (int ex) {
+//        cerr << "Integer error caught!" << endl;
+//    }
+//
+//    if (den == 0)
+//        throw string("Cannot divide by zero!\n");
+//    else
+//        return num / den;
+//}
--- 16_5.cpp
+++ 16_5.cpp
@@ -16,23 +16,23 @@ S multiply(S a, S b) {
     return a * b;
 }
 
-//template <typename T>
-//T max(T a, T b) {
-//    return (a > b) ? a: b;
-//}
+template <typename T>
+T max(T a, T b) {
+    return (a > b) ? a: b;
+}
 
 int main() {
     int a =2;
     int b = 3;
 
-    cout << "Multiply: " << multiply(a , b) << endl;
-//    double doubleA {1};
-//    double doubleB {2};
-//    char charA = 'A';
-//    char charB = 'B';
-//    cout << "Integer: " << max(a, b) << endl;
-//    cout << "Double: " << max(doubleA, doubleB) << endl;
-//    cout << "Char: " << max(charA, charB) << endl;
+    cout << "Multiply: " << multiply(2 , 3) << endl;
+    double doubleA {1};
+    double doubleB {2};
+    char charA = 'A';
+    char charB = 'B';
+    cout << "Integer: " << max(a, b) << endl;
+    cout << "Double: " << max(doubleA, doubleB) << endl;
+    cout << "Char: " << max(charA, charB) << endl;
 
     return 0;
 }
