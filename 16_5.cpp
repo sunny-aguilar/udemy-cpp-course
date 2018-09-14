db543080819e339226641da9d673ce6d0c448757
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-14 15:04:02 -0700

    add includes

--- 16_5.cpp
+++ 16_5.cpp
@@ -3,4 +3,10 @@
 ** Date:            September 13, 2018
 ** Description:     Exceptions, Templates, and the STL
 *********************************************************************/
-
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::cerr;
