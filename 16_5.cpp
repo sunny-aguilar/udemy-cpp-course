24ca6d0ea510b1121856a4376f418be505f25920
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-14 16:04:21 -0700

    removed testing with ints

--- 16_5.cpp
+++ 16_5.cpp
@@ -19,7 +19,7 @@ T square(T num1, T num2) {
 int main() {
     int a = 2;
     int b = 2;
-    cout << "Square is: " << square<int>(a,b);
+    cout << "Square is: " << square(a,b);
 
     return 0;
 }
