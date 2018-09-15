bbf7104f8cbebdc6cab62c1f743046cb36289161
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-15 15:23:49 -0700

    add vector

--- 16_6.cpp
+++ 16_6.cpp
@@ -3,4 +3,14 @@
 ** Date:            September 15, 2018
 ** Description:     Exceptions, Templates, and the STL
 *********************************************************************/
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
 
+int main() {
+    vector<int> vec {10, 20, 30,40,50};
+
+    return 0;
+}
