8675e8f9a357447de1665b7fdc3917a06536af0e
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-16 12:36:42 -0700

    add comments

--- 17_1.cpp
+++ 17_1.cpp
@@ -13,9 +13,13 @@ struct ListNode {
 };
 
 int main() {
+    // create a pointer to a ListNode structure and assign it to nullptr
     ListNode *head = nullptr;
+    // dynamically create a ListNode structure and assign it to head pointer
     head = new ListNode;
+    // assign data member in the structure pointed to by head a value
     head->value = 12.5;
+    // display value
     cout << "List value " << head->value << endl;
 
     return 0;
