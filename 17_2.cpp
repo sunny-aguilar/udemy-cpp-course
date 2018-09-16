8e07e45817d0d5649e4fc1119406fbb358b68430
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-16 13:23:21 -0700

    add constructor to struct

--- 17_2.cpp
+++ 17_2.cpp
@@ -1,4 +1,24 @@
-//
-// Created by Sandro Aguilar on 9/16/18.
-//
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            September 16, 2018
+** Description:     Linked Lists
+*********************************************************************/
+#include <iostream>
+using std::cout;
+using std::endl;
 
+struct ListNode{
+    double value;
+    ListNode *next;
+
+    ListNode(double val, ListNode *next1 = nullptr) {
+        value = val;
+        next = next1;
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
