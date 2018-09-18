78e5afe515b5bc47dbc929677676255ea66ab2d9
Sandro Aguilar <sunny.aguilar@gmail.com>
2018-09-17 19:53:09 -0700

    add class

--- 17_3.cpp
+++ 17_3.cpp
@@ -3,4 +3,28 @@
 ** Date:            September 7, 2018
 ** Description:     Linked Lists
 *********************************************************************/
+#include <iostream>
+using std::cout;
+using std::endl;
 
+class NumberList{
+    private:
+        struct ListNode {
+            double value;
+            ListNode *next;
+            ListNode(double val, ListNode *next1 = nullptr) {
+                value = val;
+                next = next1;
+            }
+        };
+        ListNode *head;
+
+    public:
+
+};
+
+int main() {
+
+
+    return 0;
+}
